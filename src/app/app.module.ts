449f932ce78819b7190a8b143bfa242567121afb
Alex Vettithanam <Alex.Vettithanam@gep.com>
2022-07-25 11:26:46 +0530

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FootBarComponent } from './foot-bar/foot-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainContentComponent,
+    FootBarComponent
   ],
   imports: [
     BrowserModule,
